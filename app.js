7efe8bd5badf0d208aa01b0f05698cd5379b3c83
Holger Bergunde <holger@bergunde.de>
2021-05-09 14:10:58 +0200

    Initial idea for adding support for multiple vacuums.

--- app.js
+++ app.js
@@ -13,22 +13,30 @@ const options = {
 const conf = new Configuration(options);
 const events = new EventEmitter();
 
-if(conf.get("mqtt")) {
-    new MqttClient({
-        brokerURL: conf.get("mqtt").broker_url,
-        caPath: conf.get("mqtt").caPath,
-        identifier: conf.get("mqtt").identifier,
-        topicPrefix: conf.get("mqtt").topicPrefix,
-        autoconfPrefix: conf.get("mqtt").autoconfPrefix,
-        mapSettings: conf.get("mapSettings"),
-        mapDataTopic: conf.get("mqtt").mapDataTopic,
-        minMillisecondsBetweenMapUpdates: conf.get("mqtt").minMillisecondsBetweenMapUpdates,
-        publishMapImage: conf.get("mqtt").publishMapImage,
-        publishMapData: conf.get("mqtt").publishMapData,
-        webserverEnabled: conf.get("webserver") && conf.get("webserver").enabled === true,
-        events: events
+if (conf.get("mqtt")) {
+
+    const robots = conf.get("mqtt").robots;
+
+    robots.forEach(robot => {
+        console.log("Starting mqtt client for " + robot.identifier)
+        new MqttClient({
+            brokerURL: conf.get("mqtt").broker_url,
+            caPath: conf.get("mqtt").caPath,
+            identifier: robot.identifier,
+            topicPrefix: robot.topicPrefix,
+            mapDataTopic: robot.mapDataTopic,
+            autoconfPrefix: conf.get("mqtt").autoconfPrefix,
+            mapSettings: conf.get("mapSettings"),
+            minMillisecondsBetweenMapUpdates: conf.get("mqtt").minMillisecondsBetweenMapUpdates,
+            publishMapImage: conf.get("mqtt").publishMapImage,
+            publishMapData: conf.get("mqtt").publishMapData,
+            webserverEnabled: conf.get("webserver") && conf.get("webserver").enabled === true,
+            events: events
+        });
     });
-    if(conf.get("webserver") && conf.get("webserver").enabled === true) {
+
+
+    if (conf.get("webserver") && conf.get("webserver").enabled === true) {
         new WebServer({
             port: conf.get("webserver").port,
             events: events
